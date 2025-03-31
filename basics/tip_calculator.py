753462cd54c759a5e72b2801e2dde8944a26b6af
tgarland-macs <tgarland@macs.vic.edu.au>
2025-04-01 10:17:36 +1100

    Updated tip_calculator.py

--- basics/tip_calculator.py
+++ basics/tip_calculator.py
@@ -1,32 +1,46 @@
 def tip_data():
     # User to enter total bill amount
-    bill_amount = input("Enter the total bill amount: ")
+    try:
+        bill_amount = float(input("Enter the total bill amount: "))
+    except ValueError:
+        print("Invalid bill amount. Please try again.")
+        return None
 
     # User to enter tip percentage
-    tip = input("Enter the tip percentage (e.g., 5%, 10%, 15%): ")
-    tip = tip.strip('%')
+    try:
+        tip = float(input("Enter the tip percentage (e.g., 5%, 10%, 15%): ").replace("%", ""))
+    except ValueError:
+        print("Tip amount must not include '%' sign.")
+        return None
 
     # User to enter number of users to divide the bill among
-    number_of_people = input("Enter the number of people splitting the bill: ")
+    try:
+        number_of_people = int(input("Enter the number of people splitting the bill: "))
+    except ValueError:
+        print("Invalid number of people. Please enter a whole number, e.g., '4'.")
+        return None
 
     return bill_amount, tip, number_of_people
 
+
 # Function to calculate the tip
-def tip_calculator(bill_amount, tip, number_of_people):
-    bill_amount = float(bill_amount)
-    tip = "1." + tip
-    tip = float(tip)
-    number_of_people = int(number_of_people)
+def tip_calculation(bill_amount, tip, number_of_people):
+    # Calculate total bill, including tip
+    total_cost = bill_amount + (bill_amount * (tip / 100))
 
-    split_bill = bill_amount * tip
+    # Calculate how much each person has to contribute to the bill
+    split_bill_amount = total_cost / number_of_people
 
-    divide_by_number_of_people = split_bill / number_of_people
+    return split_bill_amount
 
-    return divide_by_number_of_people
+def tip_calculator():
+    # Run the tip_data function
+    bill_amount, tip, number_of_people = tip_data()
+    # Run the tip_calculation function
+    split_bill_amount = tip_calculation(bill_amount, tip, number_of_people)
+    print(f"Each person must pay ${split_bill_amount:.2f}")
 
 
 if __name__ == '__main__':
-    bill_amount, tip, number_of_people = tip_data()
-    divide_by_number_of_people = tip_calculator(bill_amount, tip, number_of_people)
-    final_amount = divide_by_number_of_people
-    print(f"Each person should pay: ${final_amount:.2f}")
+    # Start the tip_calculator app
+    tip_calculator()
